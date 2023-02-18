cbe89633ebc3766db5b79b2b4026b376b7501d83
iamAbasu7 <antar.basu@zeeve.io>
2023-02-18 22:24:22 +0530

    new commit

--- runZomienet.js
+++ runZomienet.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('node:child_process').exec);
 
 module.exports = {
   zombienetRun: async function () {
-    const { stdout, stderr } = await exec('cd .. && cd parity-zeeve-zombienet/zombienet/javascript/packages/cli && node dist/cli.js -p podman spawn ../../../examples/0001-small-network.toml -d my-zombienet');
+    const { stdout, stderr } = await exec('cd .. && cd parity-zeeve-zombienet/zombienet/javascript/packages/cli && node dist/cli.js -p podman spawn /home/ubuntu/parity-zombienet/zombienet/examples/0001-small-network.toml -d my-zombienet');
     console.log(stdout);
     // console.log('stdout:', stdout);
     // console.error('stderr:', stderr);
